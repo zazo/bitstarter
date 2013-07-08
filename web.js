0b7c621803646edb90c152dc7f84fd2b0da5fde4
Anthony Zazo <anthonyzazo@gmail.com>
2013-07-08 02:04:40 +0000

    Updating web.js with buffer

--- web.js
+++ web.js
@@ -3,9 +3,11 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
 var fs = require('fs');
-var buffer = new buffer ();
-  response.send(buffer.toString('utc 8', fs.readFileSync("index.html")))});
- });
+
+var buf = new Buffer (fs.readFileSync("index.html", "utf-8"));
+
+    response.send(buf.toString());
+});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
