b87efb36bc749a9ad2021ad194dcf68025eff2ac
Anthony Zazo <anthonyzazo@gmail.com>
2013-07-08 01:58:26 +0000

    Updated web.js with buffer

--- web.js
+++ web.js
@@ -1,14 +1,13 @@
 var express = require('express');
-
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
 var fs = require('fs');
 var buffer = new buffer ();
-  response.send(buffer.toString(fs.readFileSync("index.html")))});
-});
+  response.send(buffer.toString('utc 8', fs.readFileSync("index.html")))});
+ });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+console.log("Listening on " + port);
 });
