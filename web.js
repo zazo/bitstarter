95ae557acd9c3d6c72c1364677f3327e5406c8ee
Anthony Zazo <anthonyzazo@gmail.com>
2013-07-21 20:23:35 +0000

    Updated product description.

--- web.js
+++ web.js
@@ -9,7 +9,7 @@ var buf = new Buffer (fs.readFileSync("index.html", "utf-8"));
     response.send(buf.toString());
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
 console.log("Listening on " + port);
 });
